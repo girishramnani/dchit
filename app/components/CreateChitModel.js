cc66fff769014f89aef2e31b6e38bf09b0e7076e
codecrunch <codecrunch360@gmail.com>
2018-08-12 16:11:22 +0530

    change create forum

--- app/components/CreateChitModel.js
+++ app/components/CreateChitModel.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
-
-class Example extends React.Component {
+import {Button, ButtonToolbar,Input,Modal} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+class CreateChitModel extends React.Component {
     constructor(props, context) {
       super(props, context);
   
@@ -9,19 +10,71 @@ class Example extends React.Component {
       this.handleClose = this.handleClose.bind(this);
   
       this.state = {
-        show: false
+        show: false,
+        chit:{
+          chit_name:"",     
+          chit_value:"",     
+          chit_fee_per_cent:"",     
+          member_count:""
+        }  
       };
     }
   
+   
     handleClose() {
       this.setState({ show: false });
     }
-  
-    handleShow() {
+
+    handleShow(e) {
       this.setState({ show: true });
     }
   
-    render() {
+    handleCreateChitfund() {
+      this.setState({ show: true });
+    }
+
+
+    _handleClick = async () => {
+
+      this.setState({ isLoading: true });
+      const { username, description } = this.state;
+  
+      try {
+        
+        // set up our contract method with the input values from the form
+        // get a gas estimate before sending the transaction
+        const createAccount = DTwitter.methods.create_chit(this.state.chit.chit_name,this.state.chit.chit_value,this.state.chit.fee_per_cent,this.state.chit.member_count);
+  
+        // get a gas estimate before sending the transaction
+        const gasEstimate = await createAccount.estimateGas({ from: web3.eth.defaultAccount });
+       
+        // send the transaction to create an account with our gas estimate
+        // (plus a little bit more in case the contract state has changed).
+        const result = await createAccount.send({ from: web3.eth.defaultAccount,  gas: gasEstimate + 1000 });
+        // send the transaction to create an account with our gas estimate
+        // (plus a little bit more in case the contract state has changed).
+  
+        // check result status. if status is false or '0x0', show user the tx details to debug error
+        if (result.status && !Boolean(result.status.toString().replace('0x', ''))) { // possible result values: '0x0', '0x1', or false, true
+          return this.setState({ isLoading: false, error: 'Error executing transaction, transaction details: ' + JSON.stringify(result) });
+        }
+  
+        // Completed of async action, set loading state back
+        this.setState({ isLoading: false });
+  
+        // tell our parent that we've created a user so it
+        // will re-fetch the current user details from the contract
+        this.props.onAfterUserUpdate();
+  
+        // redirect user to the profile update page
+        this.props.history.push('/update/@' + username);
+  
+      } catch (err) {
+        // stop loading state and show the error
+        this.setState({ isLoading: false, error: err.message });
+      };
+    }
+  render() {
       const popover = (
         <Popover id="modal-popover" title="popover">
           very popover. such engagement
@@ -36,16 +89,84 @@ class Example extends React.Component {
 
       return (
         <div>
-          <p>Click to get the full Modal experience!</p>
-  
-          <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
-            Launch demo modal
-          </Button>
-  
-         
+           <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header style={style.modelHeder}  closeButton>
+              <Modal.Title style={style.textModel}>
+                    Create  new ChitFund
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                    <form>
+                <div className="form-group">
+                <label>
+              Name of chitfund
+                </label>
+                <br />
+                <input
+                    name="chit_name"
+                    value={this.state.chit.chit_name}
+                    type="text"
+                     />
+                </div>
+                <div className="input-group input-group-lg">
+                <label>
+                Chit fund Amount:
+               </label>
+               <br />
+               <input
+                    name="chit_value"
+                    value={this.state.chit_value}
+                    onChange={this.handleInputChange}
+                />
+               </div>
+                <br />
+                <div className="input-group input-group-lg">
+                <label>
+                Amount per member:
+                </label>
+                <br />
+                <input
+                    name="numberOfmember"
+                    type="number"
+                    value={this.state.numberOfGuests}
+                    onChange={this.handleInputChange}
+                 />
+                </div>
+                <br />
+                <div className="input-group input-group-lg">
+                <label>
+                Presenatage of fee Organizer:
+                </label>
+                <br />
+                <input
+                    name="fee_per_cent"
+                    type="number"
+                    value={this.state.fee_per_cent}
+                    onChange={this.handleInputChange}
+                 />
+                </div>
+                <div className="input-group input-group-lg">
+                <label>
+                Member count:
+                </label>
+                <br />
+                <input
+                    name="member_count"
+                    type="number"
+                    value={this.state.member_count}
+                    onChange={this.handleInputChange}
+                 />
+                </div>
+            </form> 
+            </Modal.Body>
+            <Modal.Footer>
+            <Button onClick={this.handleSubmit}>Create New ChitFund</Button>
+              <Button onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       );
     }
   }
   
-  render(<Example />);
+  render(<CreateChitModel  />);
--- app/components/chits_list.js
+++ app/components/chits_list.js
@@ -10,18 +10,71 @@ export default class ChitsList extends React.Component {
       this.handleClose = this.handleClose.bind(this);
 
       this.state = {
-        show: false
-      };
+        show: false,
+        chit:{
+                chit_name:"",     
+                chit_value:"",     
+                chit_fee_per_cent:"",     
+                member_count:""
+            }   
+      }
     }
 
 
     handleClose() {
       this.setState({ show: false });
     }
-  
-    handleShow() {
+
+    handleShow(e) {
       this.setState({ show: true });
     }
+  
+    handleCreateChitfund() {
+      this.setState({ show: true });
+    }
+
+
+    _handleClick = async () => {
+
+      this.setState({ isLoading: true });
+      const { username, description } = this.state;
+  
+      try {
+        
+        // set up our contract method with the input values from the form
+        // get a gas estimate before sending the transaction
+        const createAccount = DTwitter.methods.create_chit(this.state.chit.chit_name,this.state.chit.chit_value,this.state.chit.fee_per_cent,this.state.chit.fee_per_cent);
+  
+        // get a gas estimate before sending the transaction
+        const gasEstimate = await createAccount.estimateGas({ from: web3.eth.defaultAccount });
+       
+        // send the transaction to create an account with our gas estimate
+        // (plus a little bit more in case the contract state has changed).
+        const result = await createAccount.send({ from: web3.eth.defaultAccount,  gas: gasEstimate + 1000 });
+        // send the transaction to create an account with our gas estimate
+        // (plus a little bit more in case the contract state has changed).
+  
+        // check result status. if status is false or '0x0', show user the tx details to debug error
+        if (result.status && !Boolean(result.status.toString().replace('0x', ''))) { // possible result values: '0x0', '0x1', or false, true
+          return this.setState({ isLoading: false, error: 'Error executing transaction, transaction details: ' + JSON.stringify(result) });
+        }
+  
+        // Completed of async action, set loading state back
+        this.setState({ isLoading: false });
+  
+        // tell our parent that we've created a user so it
+        // will re-fetch the current user details from the contract
+        this.props.onAfterUserUpdate();
+  
+        // redirect user to the profile update page
+        this.props.history.push('/update/@' + username);
+  
+      } catch (err) {
+        // stop loading state and show the error
+        this.setState({ isLoading: false, error: err.message });
+      };
+    }
+
   
     render() {
       
@@ -77,22 +130,21 @@ export default class ChitsList extends React.Component {
                 </label>
                 <br />
                 <input
-                    name="isGoing"
+                    name="chit_name"
+                    value={this.state.chit.chit_name}
                     type="text"
                      />
                 </div>
                 <div className="input-group input-group-lg">
                 <label>
-                Number of guests:
+                Chit fund Amount:
                </label>
                <br />
-               <select
-                    name="numberOfmember"
-                    value={this.state.numberOfGuests}
+               <input
+                    name="chit_value"
+                    value={this.state.chit_value}
                     onChange={this.handleInputChange}
-                >
-                <option></option>
-                </select>
+                />
                </div>
                 <br />
                 <div className="input-group input-group-lg">
@@ -110,19 +162,32 @@ export default class ChitsList extends React.Component {
                 <br />
                 <div className="input-group input-group-lg">
                 <label>
-                Amount per member:
+                Presenatage of fee Organizer:
                 </label>
                 <br />
                 <input
-                    name="numberOfmember"
+                    name="fee_per_cent"
                     type="number"
-                    value={this.state.numberOfGuests}
+                    value={this.state.fee_per_cent}
+                    onChange={this.handleInputChange}
+                 />
+                </div>
+                <div className="input-group input-group-lg">
+                <label>
+                Member count:
+                </label>
+                <br />
+                <input
+                    name="member_count"
+                    type="number"
+                    value={this.state.member_count}
                     onChange={this.handleInputChange}
                  />
                 </div>
             </form> 
             </Modal.Body>
             <Modal.Footer>
+            <Button onClick={this.handleSubmit}>Create New ChitFund</Button>
               <Button onClick={this.handleClose}>Close</Button>
             </Modal.Footer>
           </Modal>
